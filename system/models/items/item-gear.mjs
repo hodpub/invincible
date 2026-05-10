ea851a7f139caca496a7e2fc7d535f18e656155d
Cussa Mitre <cussa.mitre@gmail.com>
2026-05-10 11:16:21 +0100

    ✨ Gear item

--- system/models/items/item-gear.mjs
+++ system/models/items/item-gear.mjs
@@ -11,40 +11,9 @@ export default class InvincibleGear extends InvincibleItemBase {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
-    schema.quantity = new fields.NumberField({
-      ...requiredInteger,
-      initial: 1,
-      min: 1,
-    });
-    schema.weight = new fields.NumberField({
-      required: true,
-      nullable: false,
-      initial: 0,
-      min: 0,
-    });
-
-    // Break down roll formula into three independent fields
-    schema.roll = new fields.SchemaField({
-      diceNum: new fields.NumberField({
-        ...requiredInteger,
-        initial: 1,
-        min: 1,
-      }),
-      diceSize: new fields.StringField({ initial: 'd20' }),
-      diceBonus: new fields.StringField({
-        initial: '+@str.mod+ceil(@lvl / 2)',
-      }),
-    });
-
-    schema.formula = new fields.StringField({ blank: true });
+    schema.singleUse = new fields.BooleanField({ initial: false });
+    schema.cost = new fields.NumberField({ ...requiredInteger, initial: 1, min: 0 });
 
     return schema;
-  }
-
-  prepareDerivedData() {
-    // Build the formula dynamically using string interpolation
-    const roll = this.roll;
-
-    this.formula = `${roll.diceNum}${roll.diceSize}${roll.diceBonus}`;
   }
 }
