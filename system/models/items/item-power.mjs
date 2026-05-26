6e24125b8ae57ba4e7468432d086746caaca7340
Cussa Mitre <cussa.mitre@gmail.com>
2026-05-26 19:08:09 +0100

    ✨ Show power level on the name

--- system/models/items/item-power.mjs
+++ system/models/items/item-power.mjs
@@ -15,4 +15,26 @@ export default class InvinciblePower extends InvincibleItemBase {
 
     return schema;
   }
+
+  prepareDerivedData() {
+    super.prepareDerivedData();
+    let level = -1;
+    for (const automation of Object.values(this.automations)) {
+      const levelCheck = automation.name.toLowerCase();
+      if (!automation.showAsSelection)
+        continue;
+
+      if (levelCheck.indexOf("basic") > -1)
+        level = 0;
+      else if (levelCheck.indexOf("major") > -1)
+        level = 1;
+      else if (levelCheck.indexOf("massive") > -1)
+        level = 2;
+      else if (levelCheck.indexOf("mounstrous") > -1)
+        level = 3;
+    }
+
+    if (level > -1)
+      this.level = ["", "Major ", "Massive ", "Mounstrous "][level];
+  }
 }
